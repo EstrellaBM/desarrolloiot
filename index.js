f2adc4586f069f5f81ba3da97bf4cfc2e2ab01e2
EstrellaBM <brestrella555@gmail.com>
2025-05-21 15:23:28 -0600

    initial_commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10 °C", timestamp: new Date().toISOString() });
+});
+app.get("/otro", (req, res) => {
+  res.json({ valor: "HOLA ", timestamp: new Date().toISOString() });
+});
+app.listen(PORT, () => {
+  console.log(`Servidor corregido en puerto ${PORT}`);
+});
